0d90ac48e08e1147a3d9ee3ed6243318cc7abc2a
330吴彦祖 <windshear222@outlook.com>
2018-04-01 18:39:06 +0800

    6

--- src/render.js
+++ src/render.js
@@ -7,6 +7,45 @@ function newContainer(id){
     return div;
 }
 
+function newTotalChart(container, data){
+    let total = data.Total;
+    let atas = data.ATA;
+    let deadline = data.Deadline;
+    let decendLabel = [];
+    let decendData = [];
+    for (ata of atas){
+        decendLabel.push(ata[0]);
+        decendData.push(ata[1]);
+    }
+    let option = {
+        title: {text: `A320FM Malfunction Statics as of ${deadline} <total: ${total}>`},
+        legend: {},
+        series: [{
+            type: 'bar',
+            name: 'Count of Malfunction',
+            data: decendData
+        }],
+        xAxis: {
+            data: decendLabel,
+            axisLabel: {rotate: 45}
+        },
+        toolbox: {
+            feature: {saveAsImage:{title:'保存为图片'}},
+            right:70
+        },
+        yAxis: {},
+        tooltip: {}
+    }
+    var chart = echarts.init(container,'vintage');
+    chart.setOption(option);
+}
+
+let a320 = document.getElementById('total320');
+let a330 = document.getElementById('total330');
+newTotalChart(a320, A320);
+newTotalChart(a330, A330);
+
+
 function newChart(container, data){
     let airplane = data.Airplane;
     let deadline = data.Deadline;
@@ -18,7 +57,7 @@ function newChart(container, data){
         decendData.push(ata[1]);
     }
     let option = {
-        title: {text: `${airplane} Malfunction Statics as of ${deadline}`},
+        title: {text: `${airplane} Malfunction Statics as of ${deadline} <total: ${data.Total}>`},
         legend: {},
         series: [{
             type: 'bar',
