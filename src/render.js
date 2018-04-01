5fd2ca3573f14cd3be2644276b94c86458cdce38
330吴彦祖 <windshear222@outlook.com>
2018-04-01 22:37:47 +0800

    7

--- src/render.js
+++ src/render.js
@@ -13,27 +13,49 @@ function newTotalChart(container, data, type){
     let deadline = data.Deadline;
     let decendLabel = [];
     let decendData = [];
+    let decendPercent = [];
     for (ata of atas){
         decendLabel.push(ata[0]);
         decendData.push(ata[1]);
+        let tmp = (ata[2]*100).toString().substr(0,5);
+        decendPercent.push(tmp);
     }
     let option = {
-        title: {text: `${type} Malfunction Statics as of ${deadline} <total: ${total}>`},
-        legend: {},
-        series: [{
+        title: {text: `${type} Malfunction Statics as of ${deadline} <total: ${total}>`,
+                x:'center'
+        },
+        legend: {show: false},
+        series: [
+        {
             type: 'bar',
-            name: 'Count of Malfunction',
-            data: decendData
-        }],
+            name: 'Count',
+            data: decendData,
+            stack: 'ata',
+            label: {
+                show: true,
+                position: 'insideBottom'
+            },
+            distance: 1
+        }
+        ],
         xAxis: {
+            type: 'category',
             data: decendLabel,
-            axisLabel: {rotate: 45}
+            axisLabel: {rotate: 45},
+            axisTick: {show: false}
         },
         toolbox: {
             feature: {saveAsImage:{title:'保存为图片'}},
             right:70
         },
-        yAxis: {},
+        yAxis: [{
+            type: 'value',
+            name: 'count',
+            axisLabel: {
+                formatter: '{value} time'
+            }
+        }
+        ],
         tooltip: {}
     }
     var chart = echarts.init(container,'vintage');
@@ -44,6 +66,52 @@ let a320 = document.getElementById('total320');
 let a330 = document.getElementById('total330');
 newTotalChart(a320, A320, 'A320FM');
 newTotalChart(a330, A330, 'A330');
+
+function newTotalChartPer(container, data, type){
+    let total = data.Total;
+    let atas = data.ATA;
+    let deadline = data.Deadline;
+    let legends = [];
+    let n = [];
+    for (ata of atas){
+        n.push({value:ata[1],name:ata[0]});
+        legends.push(ata[0]);
+    }
+    let option = {
+        title: {text: `${type} Malfunction Statics as of ${deadline} <total: ${total}>`,
+                x: 'center'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'right',
+            data: legends
+        },
+        series: [
+        {
+            name: 'Percentage',            
+            type: 'pie',
+            data: n,
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+        ],
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        }
+    }
+    var chart = echarts.init(container,'vintage');
+    chart.setOption(option);
+}
+let a320p = document.getElementById('total320p');
+let a330p = document.getElementById('total330p');
+newTotalChartPer(a320p, A320, 'A320FM');
+newTotalChartPer(a330p, A330, 'A330');
 
 
 function newChart(container, data){
@@ -57,16 +125,22 @@ function newChart(container, data){
         decendData.push(ata[1]);
     }
     let option = {
-        title: {text: `${airplane} Malfunction Statics as of ${deadline} <total: ${data.Total}>`},
-        legend: {},
+        title: {text: `${airplane} Malfunction Statics as of ${deadline} <total: ${data.Total}>`,
+        x: 'center'
+        },
         series: [{
             type: 'bar',
             name: 'Count of Malfunction',
-            data: decendData
+            data: decendData,
+            label: {
+                show: true,
+                position: 'insideBottom'
+            }
         }],
         xAxis: {
             data: decendLabel,
-            axisLabel: {rotate: 45}
+            axisLabel: {rotate: 45},
+            axisTick: {show: false}
         },
         toolbox: {
             feature: {saveAsImage:{title:'保存为图片'}},
