00ba8efc7e8fe1ab2f33fda84aa53218cdf830f3
wind <windshear222@outlook.com>
2018-07-31 15:08:06 +0800

    spa

--- js/render.js
+++ js/render.js
@@ -2,13 +2,13 @@
 
 /* 渲染机队总体故障统计条形图 */
 
-function FamilyBar(container, fleet, data, theme){
+function FamilyBar(container, fleet, data, theme) {
     Update = data.update;
     Total = data.total;
     tuple_list = data.by_ata;
     ATA_Label = [];
     ATA_Count = [];
-    for(let tuple of tuple_list){
+    for (let tuple of tuple_list) {
         ATA_Label.push(tuple[0]);
         ATA_Count.push(tuple[1]);
     }
@@ -20,25 +20,25 @@ function FamilyBar(container, fleet, data, theme){
     };
     let tooltip = {
         trigger: 'axis',
-        axisPointer:{type:'shadow'}
+        axisPointer: { type: 'shadow' }
     };
     let toolbox = {
         show: true,
-        feature: {saveAsImage: {title: '保存为图片'}},
+        feature: { saveAsImage: { title: '保存为图片' } },
         left: 'center',
         top: 25,
-        iconStyle:{color: '#64BD3D', borderColor:'#64BD3D'}
+        iconStyle: { color: '#64BD3D', borderColor: '#64BD3D' }
     };
-    let  yAxis = [{
+    let yAxis = [{
         type: 'category',
         name: 'ATA章节',
-        data: ATA_Label,//list
+        data: ATA_Label, //list
         inverse: false,
     }];
     let xAxis = {
         type: 'value',
         name: '故障次数',
-        axisLabel: {formatter: '{value}次'}
+        axisLabel: { formatter: '{value}次' }
     };
     let series = [{
         type: 'bar',
@@ -48,13 +48,13 @@ function FamilyBar(container, fleet, data, theme){
             show: true,
             position: 'insideLeft'
         },
-        itemStyle:{
-            color: function(params){
+        itemStyle: {
+            color: function(params) {
                 //let colors = ['#C33531','#C33531','#C33531','#C33531','#C33531','#C33531','#C33531','#C33531','#C33531','#C33531'];
-                if(params.dataIndex < 10){
+                if (params.dataIndex < 10) {
                     //return colors[params.dataIndex];
                     return '#C33531';
-                }else{
+                } else {
                     return '#64BD3D';
                 }
             }
@@ -83,35 +83,107 @@ function FamilyBar(container, fleet, data, theme){
         xAxis: xAxis,
         yAxis: yAxis,
         series: series,
-        legend: {show: false},
+        legend: { show: false },
         graphic: graphic,
-        backgroundColor:'rgba(0, 0, 0,0.5)'
+        backgroundColor: 'rgba(0, 0, 0,0.5)'
     };
     let chart = echarts.init(container, theme);
     chart.setOption(option);
 }
 
 const A320FM = {
-    "update":"2018-07-28",
-    "total":2979,
-    "by_ata":[
-        ["ATA-21",399],["ATA-34",320],["ATA-27",280],["ATA-36",235],["ATA-32",225],["ATA-23",166],["ATA-22",163],["ATA-49",137],["ATA-30",136],["ATA-28",133],["ATA-24",77],["ATA-31",76],["ATA-79",74],["ATA-26",66],["ATA-52",61],["ATA-77",52],["ATA-29",46],["ATA-74",45],["ATA-75",41],["ATA-38",38],["ATA-35",28],["ATA-73",28],["ATA-33",22],["ATA-47",21],["ATA-25",16],["ATA-80",14],["ATA-71",14],["ATA-72",14],["ATA-46",11],["ATA-70",9],["ATA-78",9],["ATA-76",7],["ATA-57",6],["ATA-56",3],["ATA-05",2],["ATA-12",2],["ATA-55",2],["ATA-53",1]
+    "update": "2018-07-28",
+    "total": 2979,
+    "by_ata": [
+        ["ATA-21", 399],
+        ["ATA-34", 320],
+        ["ATA-27", 280],
+        ["ATA-36", 235],
+        ["ATA-32", 225],
+        ["ATA-23", 166],
+        ["ATA-22", 163],
+        ["ATA-49", 137],
+        ["ATA-30", 136],
+        ["ATA-28", 133],
+        ["ATA-24", 77],
+        ["ATA-31", 76],
+        ["ATA-79", 74],
+        ["ATA-26", 66],
+        ["ATA-52", 61],
+        ["ATA-77", 52],
+        ["ATA-29", 46],
+        ["ATA-74", 45],
+        ["ATA-75", 41],
+        ["ATA-38", 38],
+        ["ATA-35", 28],
+        ["ATA-73", 28],
+        ["ATA-33", 22],
+        ["ATA-47", 21],
+        ["ATA-25", 16],
+        ["ATA-80", 14],
+        ["ATA-71", 14],
+        ["ATA-72", 14],
+        ["ATA-46", 11],
+        ["ATA-70", 9],
+        ["ATA-78", 9],
+        ["ATA-76", 7],
+        ["ATA-57", 6],
+        ["ATA-56", 3],
+        ["ATA-05", 2],
+        ["ATA-12", 2],
+        ["ATA-55", 2],
+        ["ATA-53", 1]
     ]
 };
 
-const A330FM = {"update":"2018-07-28","total":354,"by_ata":[["ATA-21",56],["ATA-34",30],["ATA-73",30],["ATA-36",30],["ATA-23",24],["ATA-24",20],["ATA-27",20],["ATA-32",20],["ATA-28",19],["ATA-52",18],["ATA-49",17],["ATA-38",8],["ATA-47",7],["ATA-29",7],["ATA-25",6],["ATA-22",6],["ATA-74",6],["ATA-30",5],["ATA-31",5],["ATA-26",3],["ATA-33",3],["ATA-79",3],["ATA-78",3],["ATA-46",2],["ATA-71",2],["ATA-35",2],["ATA-77",1],["ATA-45",1]]};
+const A330FM = {
+    "update": "2018-07-28",
+    "total": 354,
+    "by_ata": [
+        ["ATA-21", 56],
+        ["ATA-34", 30],
+        ["ATA-73", 30],
+        ["ATA-36", 30],
+        ["ATA-23", 24],
+        ["ATA-24", 20],
+        ["ATA-27", 20],
+        ["ATA-32", 20],
+        ["ATA-28", 19],
+        ["ATA-52", 18],
+        ["ATA-49", 17],
+        ["ATA-38", 8],
+        ["ATA-47", 7],
+        ["ATA-29", 7],
+        ["ATA-25", 6],
+        ["ATA-22", 6],
+        ["ATA-74", 6],
+        ["ATA-30", 5],
+        ["ATA-31", 5],
+        ["ATA-26", 3],
+        ["ATA-33", 3],
+        ["ATA-79", 3],
+        ["ATA-78", 3],
+        ["ATA-46", 2],
+        ["ATA-71", 2],
+        ["ATA-35", 2],
+        ["ATA-77", 1],
+        ["ATA-45", 1]
+    ]
+};
 
 let x = document.getElementById('total320');
-if(x){
-FamilyBar(x, 'A320FM', A320FM, 'dark');}
+if (x) {
+    FamilyBar(x, 'A320FM', A320FM, 'dark');
+}
 x = document.getElementById('total330');
-if(x){
-FamilyBar(x, 'A330', A330FM, 'dark');
+if (x) {
+    FamilyBar(x, 'A330', A330FM, 'dark');
 }
 
-document.getElementById('btn-320').onclick = function(){
-    a320=document.getElementById('total320');
-    a330=document.getElementById('total330');
+document.getElementById('btn-320').onclick = function() {
+    document.getElementsByTagName('body')[0].style.overflow = 'auto';
+    a320 = document.getElementById('total320');
+    a330 = document.getElementById('total330');
     a320.style.visibility = 'visible';
     a320.style.display = null;
     a330.style.visibility = 'hidden';
@@ -119,9 +191,10 @@ document.getElementById('btn-320').onclick = function(){
     document.getElementById('btn-320').className = 'btn btn-success';
     document.getElementById('btn-330').className = 'btn btn-outline-primary';
 }
-document.getElementById('btn-330').onclick = function(){
-    a320=document.getElementById('total320');
-    a330=document.getElementById('total330');
+document.getElementById('btn-330').onclick = function() {
+    document.getElementsByTagName('body')[0].style.overflow = 'auto';
+    a320 = document.getElementById('total320');
+    a330 = document.getElementById('total330');
     a330.style.visibility = 'visible';
     a320.style.visibility = 'hidden';
     a330.style.display = null;
